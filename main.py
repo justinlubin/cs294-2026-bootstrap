046524023755678825fca4861408d42342cd91e5
Justin Lubin <jlubi333@gmail.com>
2026-02-20 10:59:48 -0800

    Update from class lecture

--- main.py
+++ main.py
@@ -1,48 +1,69 @@
-# %%
-
-import random  # choices
-import statistics  # quantiles, mean, median
+import random
+import statistics
 
 import matplotlib.pyplot as plt
 
-# %%
+# For real analyses, just use SciPy's bootstrap!
+#   https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.bootstrap.html
+# It also gives you a 95% confidence interval.
 
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.bootstrap.html
-# https://numpy.org/doc/stable/reference/generated/numpy.percentile.html
 
-
-def bootstrap(x, estimator, B):
-    N = len(x)
+def bootstrap(sample, estimator, B):
+    N = len(sample)
     estimates = []
-    for _ in range(0, B):
-        new_sample = random.choices(x, k=N)
+
+    for _ in range(B):
+        new_sample = random.choices(sample, k=N)
         estimate = estimator(new_sample)
         estimates.append(estimate)
+
     return estimates
 
 
-a = [1, 1.5, 0.9, 0.6, 1.3, 1.4, 1.5]
-b = [x - 0.8 for x in [2.1, 2.2, 2.1, 2, 1.7, 2.5, 2.5]]
-diffs = [x - y for x, y in zip(a, b)]
+one_second = [
+    0.78,
+    1.3,
+    1.31,
+    1.16,
+    0.96,
+    0.63,
+    0.76,
+    1.33,
+    0.86,
+    1.03,
+    0.85,
+    1.11,
+]
+
+two_second = [
+    2.15,
+    2.04,
+    1.93,
+    1.97,
+    1.96,
+    2.35,
+    2.08,
+    2.38,
+    1.71,
+    2.23,
+    2.11,
+    1.6,
+]
+
+diffs = [x - y for x, y in zip(one_second, two_second)]
 
 fig, ax = plt.subplots(1, 1)
 ax.hist(diffs)
-fig.savefig("out1.png")
+fig.savefig("empirical_distribution.png")
 
 estimator = statistics.mean
 point_estimate = estimator(diffs)
 print("point estimate:", point_estimate)
 
-estimates = bootstrap(diffs, estimator=estimator, B=10000)
-print("some bootstrap estimates:", estimates[:10])
+estimates = bootstrap(diffs, estimator, B=10000)
+print("some bootstrap estimate:", estimates[:10])
 
 fig, ax = plt.subplots(1, 1)
 ax.hist(estimates, color="0.5")
 ax.axvline(point_estimate, color="red")
-fig.savefig("out2.png")
-
-quants = statistics.quantiles(estimates, n=200)
-left = quants[5 - 1]  # 2.5 * 2, off by one
-right = quants[195 - 1]  # 97.5 * 2, off by one
-print("left endpoint:", left)
-print("right endpoint:", right)
+fig.savefig("sampling_distribution.png")
